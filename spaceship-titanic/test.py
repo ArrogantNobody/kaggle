b04cd7164b3a4c2fc14040e7ef96593836135f58
ArrogantNobody <68981858+ArrogantNobody@users.noreply.github.com>
2022-05-11 22:28:31 -0400

    binary_XGBoost

---
+++ spaceship-titanic/test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(dict(age=[5, 6, np.NaN],
+                   born=[pd.NaT, pd.Timestamp('1939-05-27'),
+                         pd.Timestamp('1940-04-25')],
+                   name=['Alfred', 'Batman', ''],
+                   toy=[None, 'Batmobile', 'Joker']))
+#print(df)
+
+mask = df['age'].isna().index
+print(mask)
+
+print(df.loc[mask])
